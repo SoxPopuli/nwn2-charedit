852284ab18ed738486ca664fd7a6f526e280619e
Charlotte Smith <charliesmith5019@gmail.com>
2025-10-16 22:57:21 +0100

    trim

--- dds/src/lib.rs
+++ dds/src/lib.rs
@@ -32,7 +32,7 @@ impl PixelFormatFlags {
 pub struct DdsPixelFormat {
     size: u32,
     flags: PixelFormatFlags,
-    four_cc: FourCC,
+    four_cc: [u8; 4],
     rgb_bit_count: u32,
     r_bit_mask: u32,
     g_bit_mask: u32,
@@ -44,7 +44,7 @@ impl DdsPixelFormat {
         Ok(Self {
             size: read!(reader)?,
             flags: PixelFormatFlags(read!(reader)?),
-            four_cc: FourCC(read!(reader)?),
+            four_cc: read!(reader)?,
             rgb_bit_count: read!(reader)?,
             r_bit_mask: read!(reader)?,
             g_bit_mask: read!(reader)?,
@@ -120,75 +120,6 @@ common::int_enum! {
 common::open_enum! {
   pub enum DXGIFormat: u32 {
     Unknown = 0,
-    R32G32B32A32_TYPELESS = 1,
-    R32G32B32A32_FLOAT = 2,
-    R32G32B32A32_UINT = 3,
-    R32G32B32A32_SINT = 4,
-    R32G32B32_TYPELESS = 5,
-    R32G32B32_FLOAT = 6,
-    R32G32B32_UINT = 7,
-    R32G32B32_SINT = 8,
-    R16G16B16A16_TYPELESS = 9,
-    R16G16B16A16_FLOAT = 10,
-    R16G16B16A16_UNORM = 11,
-    R16G16B16A16_UINT = 12,
-    R16G16B16A16_SNORM = 13,
-    R16G16B16A16_SINT = 14,
-    R32G32_TYPELESS = 15,
-    R32G32_FLOAT = 16,
-    R32G32_UINT = 17,
-    R32G32_SINT = 18,
-    R32G8X24_TYPELESS = 19,
-    D32_FLOAT_S8X24_UINT = 20,
-    R32_FLOAT_X8X24_TYPELESS = 21,
-    X32_TYPELESS_G8X24_UINT = 22,
-    R10G10B10A2_TYPELESS = 23,
-    R10G10B10A2_UNORM = 24,
-    R10G10B10A2_UINT = 25,
-    R11G11B10_FLOAT = 26,
-    R8G8B8A8_TYPELESS = 27,
-    R8G8B8A8_UNORM = 28,
-    R8G8B8A8_UNORM_SRGB = 29,
-    R8G8B8A8_UINT = 30,
-    R8G8B8A8_SNORM = 31,
-    R8G8B8A8_SINT = 32,
-    R16G16_TYPELESS = 33,
-    R16G16_FLOAT = 34,
-    R16G16_UNORM = 35,
-    R16G16_UINT = 36,
-    R16G16_SNORM = 37,
-    R16G16_SINT = 38,
-    R32_TYPELESS = 39,
-    D32_FLOAT = 40,
-    R32_FLOAT = 41,
-    R32_UINT = 42,
-    R32_SINT = 43,
-    R24G8_TYPELESS = 44,
-    D24_UNORM_S8_UINT = 45,
-    R24_UNORM_X8_TYPELESS = 46,
-    X24_TYPELESS_G8_UINT = 47,
-    R8G8_TYPELESS = 48,
-    R8G8_UNORM = 49,
-    R8G8_UINT = 50,
-    R8G8_SNORM = 51,
-    R8G8_SINT = 52,
-    R16_TYPELESS = 53,
-    R16_FLOAT = 54,
-    D16_UNORM = 55,
-    R16_UNORM = 56,
-    R16_UINT = 57,
-    R16_SNORM = 58,
-    R16_SINT = 59,
-    R8_TYPELESS = 60,
-    R8_UNORM = 61,
-    R8_UINT = 62,
-    R8_SNORM = 63,
-    R8_SINT = 64,
-    A8_UNORM = 65,
-    R1_UNORM = 66,
-    R9G9B9E5_SHAREDEXP = 67,
-    R8G8_B8G8_UNORM = 68,
-    G8R8_G8B8_UNORM = 69,
     BC1_TYPELESS = 70,
     BC1_UNORM = 71,
     BC1_UNORM_SRGB = 72,
@@ -204,43 +135,12 @@ common::open_enum! {
     BC5_TYPELESS = 82,
     BC5_UNORM = 83,
     BC5_SNORM = 84,
-    B5G6R5_UNORM = 85,
-    B5G5R5A1_UNORM = 86,
-    B8G8R8A8_UNORM = 87,
-    B8G8R8X8_UNORM = 88,
-    R10G10B10_XR_BIAS_A2_UNORM = 89,
-    B8G8R8A8_TYPELESS = 90,
-    B8G8R8A8_UNORM_SRGB = 91,
-    B8G8R8X8_TYPELESS = 92,
-    B8G8R8X8_UNORM_SRGB = 93,
     BC6H_TYPELESS = 94,
     BC6H_UF16 = 95,
     BC6H_SF16 = 96,
     BC7_TYPELESS = 97,
     BC7_UNORM = 98,
     BC7_UNORM_SRGB = 99,
-    AYUV = 100,
-    Y410 = 101,
-    Y416 = 102,
-    NV12 = 103,
-    P010 = 104,
-    P016 = 105,
-    OPAQUE_420 = 106,
-    YUY2 = 107,
-    Y210 = 108,
-    Y216 = 109,
-    NV11 = 110,
-    AI44 = 111,
-    IA44 = 112,
-    P8 = 113,
-    A8P8 = 114,
-    B4G4R4A4_UNORM = 115,
-    P208 = 130,
-    V208 = 131,
-    V408 = 132,
-    SAMPLER_FEEDBACK_MIN_MIP_OPAQUE = 189,
-    SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE = 190,
-    FORCE_UINT = 0xffffffff
   }
 }
 
@@ -267,38 +167,6 @@ impl HeaderExtra {
     }
 }
 
-#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub struct FourCC(pub [u8; 4]);
-impl std::fmt::Debug for FourCC {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use std::fmt::Write;
-
-        let chars = self
-            .0
-            .map(|x| unsafe { char::from_u32_unchecked(x as u32) });
-
-        if chars
-            .iter()
-            .all(|x| x.is_alphanumeric() || x.is_ascii_whitespace() || x.is_ascii_punctuation())
-        {
-            write!(f, "\"")?;
-
-            for c in chars {
-                f.write_char(c)?;
-            }
-
-            write!(f, "\"")
-        } else {
-            self.0.fmt(f)
-        }
-    }
-}
-impl PartialEq<[u8; 4]> for FourCC {
-    fn eq(&self, other: &[u8; 4]) -> bool {
-        self.0.eq(other)
-    }
-}
-
 #[repr(C)]
 #[derive(Debug, Default, PartialEq, Eq, Clone)]
 pub struct Rgba {
@@ -322,14 +190,6 @@ mod ffi {
     use std::ffi::{c_int, c_void};
 
     unsafe extern "C" {
-        // pub unsafe fn load_dds(
-        //     file_path: *const c_char,
-        //     width: *mut c_int,
-        //     height: *mut c_int,
-        //     four_cc: *mut c_uint,
-        //     compressed_data: *const *const c_void,
-        // );
-
         pub unsafe fn bcdec_bc7(
             compressed_block: *const c_void,
             decompressed_block: *mut c_void,
@@ -340,7 +200,7 @@ mod ffi {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Dds {
-    pub four_cc: FourCC,
+    pub four_cc: [u8; 4],
     pub header: Header,
     pub header_extra: Option<HeaderExtra>,
     pub pixels: Vec<Rgba>,
@@ -350,7 +210,7 @@ impl Dds {
     where
         R: Read,
     {
-        let four_cc = FourCC(read!(reader)?);
+        let four_cc = read!(reader)?;
         let header = Header::read(&mut reader)?;
 
         let header_extra = if header.pixel_format.flags.has_flag(PixelFormatFlags::FourCC)
